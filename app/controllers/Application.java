05ee7effce951f09df355dea43f4bbd5ef768755
CORY SIMON <cory.simon@utah.edu>
2016-05-15 23:31:18 -0600

    Home page

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,6 +1,7 @@
 package controllers;
 
 import play.*;
+import play.Play;
 import play.mvc.*;
 
 import java.util.*;
@@ -8,9 +9,17 @@ import java.util.*;
 import models.*;
 
 public class Application extends Controller {
+	
+	@Before
+	static void addDefaults() {
+	    renderArgs.put("blogTitle", Play.configuration.getProperty("blog.title"));
+	    renderArgs.put("blogBaseline", Play.configuration.getProperty("blog.baseline"));
+	}
 
     public static void index() {
-	render();
+    	Post frontPost = Post.find("order by postedAt desc").first();
+    	List<Post> olderPosts = Post.find("order by postedAt desc").from(1).fetch(10);
+    	render(frontPost, olderPosts);
     }
 
 }
